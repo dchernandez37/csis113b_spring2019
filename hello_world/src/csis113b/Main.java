fc33fc34def7e638d7f622a425b352c2c64cc6d7
davidhernandez <dchernandez@gmail.com>
2018-12-30 13:24:11 -0800

    updated the program with comments

--- hello_world/src/csis113b/Main.java
+++ hello_world/src/csis113b/Main.java
@@ -1,10 +1,28 @@
+/*
+* Student Name
+* Student ID
+* Date project started
+* Assignment # - brief description of the assignment
+* */
 package csis113b;
 
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
+/**
+ * Every java program contains at list one class with one static main program
+ * The class must be the same name as the .java file it is contained in
+ */
+public class Main
+{
+    /**
+     * main method
+     * static method that runs the java program
+     * @param args list of command-line arguments passed into program
+     */
+    public static void main(String[] args)
+    {
+        // declare and initialize a message to display
         String message = "hello world!";
+        
+        // display the message to the console
         System.out.println(message);
     }
 }
