f7cbaaa12db001855808c562fad44c79397fd1a0
davidhernandez <dchernandez@gmail.com>
2018-12-30 14:10:22 -0800

    adding stuff

--- journal01/src/csis113b/Main.java
+++ journal01/src/csis113b/Main.java
@@ -1,3 +1,10 @@
+/*
+ * Student Name
+ * Student ID
+ * December 30, 2018
+ * Journal 01 - program demonstrates use of basic math functions and variable
+ * declaration and initialization.
+ * */
 package csis113b;
 
 public class Main
@@ -5,8 +12,11 @@ public class Main
     
     public static void main(String[] args)
     {
-        // declare and initialize an integer
-        int firstNumber = 3;
+        // declare an integer variable
+        int firstNumber;
+        
+        // assign the value 3 to the variable
+        firstNumber = 3;
         
         // declare and initialize using var
         var secondNumber = 7;
