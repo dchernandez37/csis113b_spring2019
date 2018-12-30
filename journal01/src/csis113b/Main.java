7fbd11e2b7eb96e54f4bd022e6e9e49e2e40ccd4
davidhernandez <dchernandez@gmail.com>
2018-12-30 14:01:58 -0800

    adding stuff

---
+++ journal01/src/csis113b/Main.java
@@ -0,0 +1,13 @@
+package csis113b;
+
+public class Main
+{
+    
+    public static void main(String[] args)
+    {
+        // declare and initialize an integer
+        int firstNumber = 3;
+        
+        
+    }
+}
