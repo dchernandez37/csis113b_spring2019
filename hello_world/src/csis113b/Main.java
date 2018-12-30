b7a34bce45b39e4e7a809e3fe7ea766c0fa4a5ec
davidhernandez <dchernandez@gmail.com>
2018-12-30 13:14:27 -0800

    Adding in the project files

---
+++ hello_world/src/csis113b/Main.java
@@ -0,0 +1,10 @@
+package csis113b;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String message = "hello world!";
+        System.out.println(message);
+    }
+}
